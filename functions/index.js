2c13b34d260f87e3ad34fd48a7c0638da886ea3a
Tzahile <Tzahile@users.noreply.github.com>
2020-09-13 15:53:43 +0300

    firebase functions - connection to sheets

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,107 @@
 const functions = require("firebase-functions");
+// const admin = require("firebase-admin");
+const { google } = require("googleapis");
+const sheets = google.sheets("v4");
+const _ = require("lodash");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const spreadsheetId = "16Y2EyQrN4dnG5quEvYty4IGR3kLuKHItDf4r557PptI";
+// const app = admin.initializeApp();
+// const db = admin.database(app);
+const serviceAccount = require("./match-4-help-firebase-adminsdk-lhr2l-9b85dfb98d.json");
+
+const jwtClient = new google.auth.JWT({
+  email: serviceAccount.client_email,
+  key: serviceAccount.private_key,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
+
+const jwtAuthPromise = jwtClient.authorize();
+
+const flatten = (data) => {
+  var result = {};
+  const recurse = function (cur, prop) {
+    if (Object(cur) !== cur) {
+      result[prop] = cur;
+    } else if (Array.isArray(cur)) {
+      const length = cur.length;
+      for (var i = 0; i < length; i++) recurse(cur[i], prop + "[" + i + "]");
+      if (l === 0) result[prop] = [];
+    } else {
+      var isEmpty = true;
+      for (var p in cur) {
+        isEmpty = false;
+        recurse(cur[p], prop ? prop + "." + p : p);
+      }
+      if (isEmpty && prop) result[prop] = {};
+    }
+  };
+  recurse(data, "");
+  return result;
+};
+// console.log(functions.region("europe-west3").database);
+// exports.SyncToSheets1 = functions
+//   .region("europe-west3")
+//   .database.refFromURL(
+//     "https://match-4-help.firebaseio.com/" + spreadsheetId + "/"
+//   )
+//   .onUpdate((change, context) => {
+//     console.log("function 1");
+//     console.log(change);
+//     console.log(context);
+//   });
+
+exports.SyncToSheets4 = functions
+  .region("europe-west3")
+  .database.ref(spreadsheetId + "/Match4Help")
+  .onUpdate(async (change, context) => {
+    console.log("function 4");
+    functions.logger.debug("Entered!");
+    functions.logger.log("Entered!");
+    functions.logger.info("Entered!");
+    functions.logger.log("These are run logs:");
+
+    const jsonData = change.after.val();
+
+    functions.logger.log(jsonData);
+
+    let allKeys = Object.keys(jsonData);
+    let firstElement = allKeys[0] || 0;
+    let headerData = flatten(jsonData[firstElement]);
+    let keys = Object.keys(headerData);
+
+    functions.logger.log("headerData keys:");
+    functions.logger.log(headerData);
+
+    let rows = [];
+    for (const item in jsonData) {
+      let val = jsonData[item];
+      let row = [item];
+      keys.forEach((key) => {
+        row.push(_.get(val, key) || "");
+      });
+      rows.push(row);
+    }
+    let sheetData = [["id", ...keys], ...rows];
+
+    functions.logger.log("sheet data:");
+    functions.logger.log(sheetData);
+
+    let range = `Match4Help!A1:${String.fromCharCode(65 + keys.length)}${
+      allKeys.length + 1
+    }`;
+
+    functions.logger.log("range:");
+    functions.logger.log(range);
+
+    await jwtAuthPromise;
+    await sheets.spreadsheets.values.update(
+      {
+        auth: jwtClient,
+        spreadsheetId: spreadsheetId,
+        range: range,
+        valueInputOption: "RAW",
+        requestBody: { values: sheetData },
+      },
+      {}
+    );
+  });
